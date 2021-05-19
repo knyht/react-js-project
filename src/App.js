34e068ed0afce088d9ed8979715125cbd9789a42
knyht <polkarpovkirill@gmail.com>
2021-05-19 18:07:46 +0300

    correct some strange problem

--- src/App.js
+++ src/App.js
@@ -1,34 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
+import MyTodoList from './components/MyTodoList/my_todo_list'
 
 function App() {
   return (
-<<<<<<< Updated upstream
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello world!
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-=======
     <div className='app'>
       <main>
         <MyTodoList />
       </main>
     </div>
   )
->>>>>>> Stashed changes
 }
 
 export default App;
